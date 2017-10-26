756c71bc37e066b855ab959ca97b04f215b7a979
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2017-10-26 13:02:53 +0200

    Added support for multiple tables in database functions.

--- timer.py
+++ timer.py
@@ -18,22 +18,33 @@ def teardown_db(connection):
     connection.close()
 
     
-def generate_db():
+def generate_db(table = None):
     cursor, connection = setup_db()
 
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS time"
-        "(start REAL PRIMARY KEY, stop REAL, duration INTEGER)"
-    )
+    if not table:
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS time"
+            "(start REAL PRIMARY KEY, stop REAL, duration INTEGER)"
+        )
+    else:
+        cursor.execute(
+            (
+                "CREATE TABLE IF NOT EXISTS {0}"
+                "(start REAL PRIMARY KEY, stop REAL, duration INTEGER)"
+            ).format(table)
+        )
 
     teardown_db(connection)
 
 
-def read_db():
+def read_table(table = None):
     cursor, connection = setup_db()
     
     dblist = []
-    cursor.execute('SELECT * FROM time')
+    if not table:
+        cursor.execute('SELECT * FROM time')
+    else:
+        cursor.execute('SELECT * FROM {0}'.format(table))
     data = cursor.fetchall()
     for row in data:
         dblist.append(row)
@@ -42,17 +53,23 @@ def read_db():
     return dblist
 
     
-def enter_record(start, stop, duration):
+def enter_record(start, stop, duration, table = None):
     cursor, connection = setup_db()
 
-    cursor.execute(
-        "INSERT INTO time VALUES (?, ?, ?)",
-        (start, stop, duration)
-    )
+    if not table:
+        cursor.execute(
+            "INSERT INTO time VALUES (?, ?, ?)",
+            (start, stop, duration)
+        )
+    else:
+        cursor.execute(
+            "INSERT INTO {0} VALUES (?, ?, ?)".format(table),
+            (start, stop, duration)
+        )
 
     teardown_db(connection)
 
-    
+
 def worktime():
     start = time.time()
     input("Press enter when finished.\n")
@@ -69,7 +86,7 @@ def main():
     enter_record(stamp[0], stamp[1], stamp[2])
     
     total_seconds = 0
-    for row in read_db():
+    for row in read_table():
         #print(row)
         total_seconds += row[2]
         
