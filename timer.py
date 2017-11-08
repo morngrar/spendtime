99d2e5e8b2602648c92969d4938228bb880a5783
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2017-11-08 18:45:05 +0100

    Moved ui to it's own library, fixing an infinite loop bug, and added menu.

--- timer.py
+++ timer.py
@@ -6,6 +6,10 @@ import sys
 import logging
 import os
 
+# Imports with dependencies
+import ui
+
+
 try:
     os.remove("timetable.log")
 except:
@@ -13,8 +17,6 @@ except:
 
 # Uncomment if debug log is needed
 #logging.basicConfig(filename="timetable.log", level=logging.DEBUG)
-
-import ui
 
 DATABASE_FILE = "worktime.db"
 
@@ -91,6 +93,9 @@ def list_tables():
     teardown_db(connection)
     return returnlist
 
+def delete_table(tablename):
+    ui.show("This is where a table will be deleted.")
+
 def worktime():
     start = time.time()
     input("Press enter when finished.\n")
@@ -98,23 +103,84 @@ def worktime():
     duration = round(stop - start)
     return (start, stop, duration)
 
+def total_seconds(table):
+    seconds = 0
+    for row in table:
+        seconds += row[2]
+    return seconds
+
 def run_worktime(table = None):
     stamp = worktime()
     print("Time spent: ", round(stamp[2]/60, 1), "min\n")
     enter_record(stamp[0], stamp[1], stamp[2], table)
 
-    total_seconds = 0
-    for row in read_table(table):
-        total_seconds += row[2]
-
-    print("In total ", round(total_seconds/3600, 2), " hours.")
+    print("In total ", round(total_seconds(read_table(table))/3600, 2), " hours.")
     time.sleep(3)
 
 
 def menu():
     # TODO: Menu for managing tables and seeing total time spent in each
-    print(list_tables())
+    heading = ui.underline("Main menu")
+    options = [
+        {"key":"l", "text":"List tables", "function":menu_list_tables},
+        {"key":"n", "text":"Create new table", "function":menu_new_table}
+    ]
+    ui.menu(options, heading)
 
+def menu_list_tables():
+    tables = list_tables()
+    heading = ui.underline("Available timetables")
+    options = []
+    i = 0
+
+    if tables:
+        for table in tables:
+            i += 1
+            options.append(
+                {
+                    "key":str(i),
+                    "text":table,
+                    "function":lambda:menu_table(table)
+                }
+            )
+    options.append(
+        {
+            "key":"n",
+            "text":"New table",
+            "function":menu_new_table
+        }
+    )
+#    if ui.menu(options, heading):
+#        return 1
+    ui.menu(options, heading)
+
+def menu_new_table():
+    tablename = input("\nName of new table: ")
+    generate_db(table = tablename)
+    menu_table(tablename)
+
+def menu_table(tablename):
+    heading = ui.underline(tablename)
+    table = read_table(tablename)
+
+    info = (
+        "Total time spent: {0} hours\n"
+    ).format(round(total_seconds(table)/3600, 2))
+
+    options = [
+        {
+            "key":"s",
+            "text":"Spend time on this",
+            "function":lambda:this_table(tablename)
+        },
+        {
+            "key":"d",
+            "text":"Delete this table",
+            "function":lambda:delete_table(tablename)
+        }
+    ]
+    ui.menu(options, heading + info)
+    return 1
 
 def this_table(text = None):
     """Run worktime with specific table. Fails on non-existing table"""
@@ -129,6 +195,7 @@ def this_table(text = None):
         print("This timetable doesn't exist yet!")
         return
 
+    print("Spending time on", text)
     run_worktime(table=text)
 
 def main():
